9d2eafec777d32caf1ba81df00bc0e7fc2c3e512
Lindsay Feinberg <lfeinberg@LFP-CHI.local>
2023-06-21 11:07:54 -0500

    fixed order

--- pnl_older.py
+++ pnl_older.py
@@ -11,7 +11,7 @@ import calendar
 from openpyxl import Workbook
 import datetime
 from datetime import datetime
-from pandas.tseries.offsets import BMonthEnd, BDay, CustomBusinessDay
+from pandas.tseries.offsets import BMonthEnd
 
 def last_business_days(start_date, end_date):
     start_date = pd.to_datetime(start_date).date()
@@ -34,12 +34,21 @@ assign_exer_df = trade_df[(trade_df.type == 'ASSIGN') | (trade_df.type == 'EXER'
 
 pnl_dict = {}
 unique_trade_dates = pd.unique(stock_option_df.date)
+extra_dates = [datetime(2019, 10, 31), datetime(2019, 11, 29), 
+                                datetime(2019, 12, 31), datetime(2020, 1, 31),
+                                datetime(2020, 4, 30), datetime(2020, 5, 29), 
+                                datetime(2020, 7,31), datetime(2020, 10, 30)]
+extra_dates = pd.Series(extra_dates)
 unique_trade_dates = pd.Series(unique_trade_dates)
+unique_trade_dates = pd.concat([extra_dates, unique_trade_dates])
+unique_trade_dates = unique_trade_dates.sort_values()
 
 s_date = datetime(2019, 9, 1)
 e_date = datetime(2022, 8, 31)
 
 d_list = last_business_days(s_date, e_date)
+d_list.append(datetime(2021, 5, 28))
+monthly_dict = {}
 
 for date in unique_trade_dates:
     day_trades = stock_option_df[stock_option_df.date == date]
@@ -59,26 +68,38 @@ for date in unique_trade_dates:
                 ((pnl_dict[tick][1] <= 0) and (day_trades.at[index, 'quantity'] < 0))):
                 new_weighted_ave = ((current_weighted_ave*current_position + quantity*day_trades.at[index, 'price']) 
                                     / new_position)
+                
                 pnl_dict[tick] = [new_weighted_ave, new_position, current_pnl]
+                
             else:
                 new_pnl = (-day_trades.at[index, 'price'] + current_weighted_ave)*(quantity)
                 pnl_dict[tick] = [current_weighted_ave, new_position, new_pnl]
         else:
             pnl_dict[tick] = [day_trades.at[index, 'price'], quantity, 0]
+  
     if date in d_list:
-        print(date)
-        if date == datetime(2021, 5, 31):
-            date = datetime(2021, 5, 28)
+        pnl_copy = pnl_dict.copy()
         for ticker in pnl_dict.keys():
             if str(ticker) != 'nan':
-               current_weighted_ave = pnl_dict[ticker][0]
-               current_position = pnl_dict[ticker][1]
-               current_pnl = pnl_dict[ticker][2]
+               current_weighted_ave = pnl_copy[ticker][0]
+               current_position = pnl_copy[ticker][1]
+               current_pnl = pnl_copy[ticker][2]
                new_price = bloomberg_df.at[date, ticker]
                if str(new_price) == 'nan':
-                   pnl_dict[ticker] = [current_weighted_ave, current_position, (current_pnl)]
+                   pnl_copy[ticker] = [current_weighted_ave, current_position, (current_pnl)]
                else:   
                    new_pnl = current_pnl + bloomberg_df.at[date, ticker] - current_weighted_ave
-                   pnl_dict[ticker] = [new_price, current_position, new_pnl]
-              
+                   pnl_copy[ticker] = [current_weighted_ave, current_position, new_pnl]
+        monthly_dict[date] = pnl_copy
+        
+
+        for ticker in pnl_dict.keys():
+            if str(ticker) != 'nan':
+               current_position = pnl_copy[ticker][1]
+               new_price = bloomberg_df.at[date, ticker]
+               if str(new_price) == 'nan':
+                   pnl_dict[ticker] = [current_weighted_ave, current_position, 0]
+               else:   
+                   pnl_dict[ticker] = [new_price, current_position, 0]
+         
    
